529789fc7ea904e2f07d30e3f16015a3bd76fd30
stan <99041312@mydavinci.nl>
2018-06-18 10:38:14 +0200

    webserver
    verder

--- routes/web.php
+++ routes/web.php
@@ -45,28 +45,3 @@ Route::get('sendemail', function () {
     return "Your email has been sent successfully";
 
 });
-
-// <?php
-// use function foo\func;
-// /*
-// |--------------------------------------------------------------------------
-// | Web Routes
-// |--------------------------------------------------------------------------
-// |
-// | Here is where you can register web routes for your application. These
-// | routes are loaded by the RouteServiceProvider within a group which
-// | contains the "web" middleware group. Now create something great!
-// |
-// */
-// Route::get('/', function () {
-//     return view('auth/login');
-// });
-// Route::get('/student', function () {
-//     return view('student');
-// });
-// Route::get('/docent', function () {
-//     return view('auth/login');
-// });
-// Route::get('/fun/hi/{id}', 'FunController@Hi');
-// Auth::routes();
-// Route::get('/home', 'HomeController@index')->name('home');
