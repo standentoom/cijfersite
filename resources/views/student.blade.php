f8bf3bf54f485247ef4f3a27e1f7ea6502697fa5
JordyLock <99039425@mydavinci.nl>
2018-06-20 10:43:14 +0200

    Fixed navbar signed in

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,20 +1,28 @@
 @extends('adminlte::master')
 
-
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css">
     
     <title>Cijfers</title>
 </head>
 
-
 @section('body')
-   
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Cijfersite" src="...">
+      </a>
+    </div>
+    <p class="navbar-text navbar-right">Signed in as Jordy</p>
+  </div>
+</nav>
+
+
     <table class="display data-table">
         <thead>
             <tr>
